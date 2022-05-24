e3c91069d789156d660cbe087f6dcf116d10db26
Antonia-Maria <99595761+Antonia-Maria@users.noreply.github.com>
2022-05-24 19:41:52 +0300

    Delete laravel directory

--- laravel/storage/Project Files/documenteDosar.php
+++
@@ -1,7 +0,0 @@
-
-<html>
-<button type="button">EDITARE</button>
-<button type="button">DESCARCA</button>
-<button type="button">STERGE</button>
-</html>
-
--- laravel/storage/Project Files/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-setcookie("username", $_POST['username'], time() - 3600, "/", "", 0);
-
-header("location: logare.php");
-?>
